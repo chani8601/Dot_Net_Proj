f80e626c59dbe2d4570a8625815603a45d71cd06
Brachi Lubling <brachil5119@email.com>
2026-04-28 00:48:38 +0300

    Adding entities

--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BL.BO
+﻿
+//namespace BL.BO
+namespace BO
 {
-    internal class Enums
-    {
-    }
+    
 }
